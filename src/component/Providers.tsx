ece36df7d994fa590a2bdc93d6723c5758b2fca3
yashdev9274 <yashdev.yvd@gmail.com>
2024-03-14 18:35:41 +0530

    Setup Trpc

--- src/component/Providers.tsx
+++ src/component/Providers.tsx
@@ -1,5 +1,41 @@
-const Providers = () => {
-    return <div></div>
+"use client";
+
+import { trpc } from "@/app/_trpc/client"
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+import { httpBatchLink } from '@trpc/client'
+// import { absoluteUrl } from '@/lib/utils'
+
+import { PropsWithChildren, useState } from "react"
+
+const Providers = ({ children }: PropsWithChildren) => {
+
+    const [queryClient] = useState(() => new QueryClient())
+    const [trpcClient] = useState(() =>
+        trpc.createClient({
+            links: [
+                httpBatchLink({
+                    // url: absoluteUrl("/api/trpc"),
+                    url: "http://localhost:3000/api/trpc",
+
+                })
+            ]
+        })
+    )
+
+    return (
+
+        <trpc.Providers
+            client={trpcClient}
+            query={queryClient}
+        >
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </trpc.Providers>
+    )
 }
 
 export default Providers
