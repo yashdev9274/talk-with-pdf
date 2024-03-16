5e7a7c3179c8db5c1c02550affdb0eed72061638
yashdev9274 <yashdev.yvd@gmail.com>
2024-03-16 19:38:38 +0530

    Update kinde auth route file

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,10 +1,14 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
     request: Request,
-    {params}:any
-){
+    { params }: any
+) {
+    // const endpoint = params.kindeAuth;
+    // return handleAuth(request, endpoint);
+
     const endpoint = params.kindeAuth;
-    return handleAuth(request, endpoint);
+    const handler = await handleAuth(request, endpoint);
+    return NextResponse.json(handler);
 } 
