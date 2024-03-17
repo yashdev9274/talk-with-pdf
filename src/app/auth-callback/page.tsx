2bdcee83b285b09ee8581e3642cdcf73952f0e27
yashdev9274 <yashdev.yvd@gmail.com>
2024-03-17 21:26:05 +0530

    Comment out trpc auth-callback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,17 +1,17 @@
-import { useRouter, useSearchParams } from "next/navigation";
-import { trpc } from "../_trpc/client";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import { trpc } from "../_trpc/client";
 
-const Page = async () => {
+// const Page = async () => {
 
-    const router = useRouter()
-    const searchParams = useSearchParams()
+//     const router = useRouter()
+//     const searchParams = useSearchParams()
 
-    const origin = searchParams.get('origin')
+//     const origin = searchParams.get('origin')
 
-    const apiResponse = await fetch('api/whatever')
-    // const data = await apiResponse.json()
+//     const apiResponse = await fetch('api/whatever')
+//     // const data = await apiResponse.json()
 
-    const { data } = trpc.test.useQuery()
-}
+//     const { data } = trpc.test.useQuery()
+// }
 
-export default Page;
+// export default Page;
