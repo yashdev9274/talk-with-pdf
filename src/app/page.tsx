7156a7a2dcfe0883907dc1666e08bce7d46e8177
yashdev9274 <yashdev.yvd@gmail.com>
2024-03-25 17:30:23 +0530

    KindeBrowserClient hook test in page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+"use client"
+
+import Image from "next/image";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
 import MaxWidthWrapper from "@/component/MaxWidthWrapper";
 import Footer from "@/component/home/Footer";
 import HowToUse from "@/component/home/HowToUse";
 import MainContainer from "@/component/home/MainContainer";
 import WeAreOpenSourced from "@/component/home/WeAreOpenSourced";
-import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const { user } = useKindeBrowserClient();
+
+  useEffect(() => {
+    console.log("--", user)
+  }, [user])
+
   return (
     <MaxWidthWrapper className="sm:p-7 sm:pb-0">
       <MainContainer />
